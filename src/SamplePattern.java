636ed98d2f8964277642c1a82146fec37b0b1bd0
Von Coleman <von.coleman@yahoo.com>
2023-10-12 22:16:45 -0500

    first commit

---
+++ src/SamplePattern.java
@@ -0,0 +1,11 @@
+
+public class SamplePattern { //The class name is SamplePattern
+    public static void main(String[] args){ //Main. Beginning of the program
+        
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a ");
+        System.out.println("J  J  aaaaa   V V  aaaaa  ");
+        System.out.println(" JJ  a     a   V  a     a ");
+        
+    }   
+}
