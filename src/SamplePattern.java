77ca876b8e438025ab84b8c5d48430ec064129e3
Von Coleman <von.coleman@yahoo.com>
2023-10-12 22:18:57 -0500

    Quick change to comments.

--- src/SamplePattern.java
+++ src/SamplePattern.java
@@ -1,9 +1,9 @@
 
 public class SamplePattern { //The class name is SamplePattern
     public static void main(String[] args){ //Main. Beginning of the program
-        
-        System.out.println("   J    a   v     v  a ");
-        System.out.println("   J   a a   v   v  a a ");
+
+        System.out.println("   J    a   v     v  a "); //this bit almost threw us for a loop. No programming puns intended. I've got a long way to go before I can do that.
+        System.out.println("   J   a a   v   v  a a "); //remember that CHARS require ""'s for them to work. Java no likey, no ""'s for CHARS.
         System.out.println("J  J  aaaaa   V V  aaaaa  ");
         System.out.println(" JJ  a     a   V  a     a ");
         
